9aa55956bf51984a68b00770e13d465c36b5c0aa
Yizhong Wang <wang.6689@osu.edu>
2023-04-17 16:29:40 -0400

    updated resolver.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,6 +5,9 @@ const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
   Query: {
+    store: async()=>{
+      return await Seller.find();
+    },
     categories: async () => {
       return await Category.find();
     },
